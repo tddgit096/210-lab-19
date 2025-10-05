9241c46823abb584547daa0688d8d378ca41352e
tddgit096-pc <>
2025-10-05 10:28:10 -0700

    work on movie class

--- main.cpp
+++ main.cpp
@@ -1,62 +1,72 @@
 //COMSC 210 | Lab 19| Toma Dimov
 //TODO
 //read review comments from external file
-//movie class w/ private members: title, linked list of structs containing ratings and reviews.
 //main() container of movie objects. can be array ,vector, linked list.
-//Nodes can be added to head for ease
+//Reviews can be added to head for ease
 //write a driver/demo program that will have at least 4 movie objs with at least 3 reviews.
 
 #include <iostream>
 using namespace std;
 
-//global variable
-bool addToHeadMode = true;
-
-struct Node{
+struct Review{
     float rating;
     string comment;
-    Node* next;
+    Review* next;
 //constructors
-    Node(){}
-    Node(float rating,string comment){this->rating=rating;this->comment=comment;next=nullptr;}
+    Review(){}
+    Review(float rating,string comment){this->rating=rating;this->comment=comment;next=nullptr;}
 };
 //prototype functions
-void promptMode();
-Node * promptData();
-void output(Node*);
+Review * promptData();
+void output(Review*);
+
+class Movie{
+    string title;
+    Review * reviews;
+
+    public:
+//setters & getters
+    void setTitle(string t){title=t;}
+    string getTitle(){return title;}
+//constructors
+    //new movie created, prompt user to input reviews.
+//output reviews
+    void outputReviews(){
+        if(!reviews){
+        cout<<"List is empty.\n";
+        return;
+    }
+    cout<<"Outputting all reviews: \n";
+    int count = 1;
+    float sum = 0;
+    Review *ReviewPtr = reviews;
+    while(ReviewPtr){
+        cout<<"Review #"<<count<<": "<<ReviewPtr->rating<<" :   \""<<ReviewPtr->comment<<"\"\n";
+        sum+=ReviewPtr->rating;
+        ReviewPtr=ReviewPtr->next;
+        count++;
+    }
+    cout<<"Average: "<<  sum/(count-1); 
+    cout<<endl;
+    }
+//input reviews:
+
+};
 
 
 int main(){
-    promptMode();
-    Node * head = promptData();
+    Review * head = promptData();
     output(head);
     return 0;
 }
 
-//prompts user on which mode to use: Append to head or append to tail
-void promptMode(){
-    int entry;
-    cout<<"Which linked list method should we use?"<<endl;
-    cout<<"\t[1] New nodes are added at the head of the linked list.\n";
-    cout<<"\t[2] New nodes are added at the tail of the linked list.\n";
-    cout<<"Choice:\t";
-    cin>>entry;
-    while(entry!=1 && entry!=2){
-        cout<<"Invalid choice. Enter 1 or 2: ";
-        cin>>entry;
-    }
-    if(entry==1)
-        addToHeadMode=true;
-    else
-        addToHeadMode=false;
-}
 //prompts user to enter ratings / comments
-Node * promptData(){
+Review * promptData(){
     float rating;
     string comment;
     char yesno;
-    Node* head = nullptr;
-    Node* nodePtr=head;
+    Review* head = nullptr;
+    Review* ReviewPtr=head;
     while(true){
         cout<<"Enter review rating 0-5: ";
         cin>> rating;
@@ -68,19 +78,15 @@ Node * promptData(){
         cin.ignore();
         getline(cin, comment);
 //check mode here:
-        Node * N = new Node(rating,comment);
+        Review * N = new Review(rating,comment);
         if(!head){
             head=N;
             N->next=nullptr;
-            nodePtr=N;
-        }
-        else if(addToHeadMode){
-            N->next = head;
-            head=N;
+            ReviewPtr=N;
         }
         else{
-            nodePtr->next = N;
-            nodePtr=nodePtr->next;
+            N->next = head;
+            head=N;
         }
         cout<<"Enter another review? Y/N: ";
         cin>>yesno;
@@ -90,7 +96,7 @@ Node * promptData(){
     }
 }
 //Outputs data in order and outputs the average of ratings.
-void output(Node* head){
+void output(Review* head){
     if(!head){
         cout<<"List is empty.\n";
         return;
@@ -98,11 +104,11 @@ void output(Node* head){
     cout<<"Outputting all reviews: \n";
     int count = 1;
     float sum = 0;
-    Node *nodePtr = head;
-    while(nodePtr){
-        cout<<"Review #"<<count<<": "<<nodePtr->rating<<" :   \""<<nodePtr->comment<<"\"\n";
-        sum+=nodePtr->rating;
-        nodePtr=nodePtr->next;
+    Review *ReviewPtr = head;
+    while(ReviewPtr){
+        cout<<"Review #"<<count<<": "<<ReviewPtr->rating<<" :   \""<<ReviewPtr->comment<<"\"\n";
+        sum+=ReviewPtr->rating;
+        ReviewPtr=ReviewPtr->next;
         count++;
     }
     cout<<"Average: "<<  sum/(count-1); 
